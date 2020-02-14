740787d07fe5dba9bf5e0dad3fb70ae0757ac1ad
Bert Beckmann <bertbeck@gmail.com>
2020-02-14 12:12:13 -0700

    update

---
+++ Assets/Scripts/Scene1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		Debug.Log("Start()");
+		NetworkMeshAnimator.Instance.StartAcceptingMessages();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log("Update()");
+	}
+}
